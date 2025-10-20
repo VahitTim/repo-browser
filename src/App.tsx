ee26d4d905588d7637c6f25c8937931c0502d8a7
Vakhitvot Timyr <vakhitovtimyr66@gmail.com>
2025-10-20 05:36:09 +0500

    Добавил repoinfo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import RepoBrowser from './components/RepoBrowser'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <RepoBrowser/>
